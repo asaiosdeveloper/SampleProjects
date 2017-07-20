b8ca9910af676feef8d8c6eed5e81b7f11fc117c
Hexalab-swift <Hexalab@Hexalab-swifts-MacBook.local>
2017-07-20 16:56:08 +0530

    Initial Commit

---
+++ iOS_Reveal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Reveal
+//
+//  Created by Hexalab on 7/20/17.
+//  Copyright © 2017 Hexalab Software Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
